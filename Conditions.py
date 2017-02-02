e1ada1d0351a60640ea8480fd06c85b78f5f8d60
nsachs <nsachs@fwparker.org>
2017-02-01 18:05:05 -0600

    initial work

--- Conditions.py
+++ Conditions.py
@@ -8,6 +8,35 @@
 # Make a series of if/elif/else statements to print the letter grade.
 # If the user enters a grade lower than zero or higher than 100, just give an error message.
 # Don't worry about making an exception for these right now.
+def grade(percent):
+    if percent >= 97 and percent <= 100:
+        print("A+")
+    elif percent >= 93 and percent <= 96:
+        print("A")
+    elif percent >= 90 and percent <= 92:
+        print("A-")
+    elif percent >= 87 and percent <= 89:
+        print("B+")
+    elif percent >= 83 and percent <= 86:
+        print("B")
+    elif percent >= 80 and percent <= 82:
+        print("B-")
+    elif percent >= 77 and percent <= 79:
+        print("C+")
+    elif percent >= 73 and percent <= 76:
+        print("C")
+    elif percent >= 70 and percent <= 72:
+        print("C-")
+    elif percent >= 67 and percent <= 69:
+        print("D+")
+    elif percent >= 65 and percent <= 66:
+        print("D")
+    if percent <= 65 and percent >= 0:
+        print("F")
+    else:
+        print("Error")
+
+grade(int(input("Enter your grade: ")))
 
 
 # PROBLEM 2 (Vowels - 5pts)
@@ -18,12 +47,13 @@
 # Try to print proper output (e.g., printing “There are 1 different vowels in the string” is ugly).
 # Example: When the user enters the string “It’s Owl Stretching Time,”
 # the program should say that there are 3 different vowels in the string
-
+'''
 def vowels(words):
     vowels = 0
 
 
 print(input("Enter a a phrase: "))
+'''
 
 # PROBLEM 3 (Quadratic Equation - 6pts)
 # You can solve quadratic equations using the quadratic formula.
@@ -38,3 +68,13 @@ print(input("Enter a a phrase: "))
 # then prints those solutions.
 # Note: Make sure that you also take into account the case that A is zero,
 # and the case that both A and B are zero.
+def quadratic_equation(a,b,c):
+    if a == 0:
+        print("Undefined")
+    elif b == 0:
+        print("The answer is 0.")
+    else:
+        x = (-b -(((b ** 2) - (4 * a * c)))** 0.5)/(2 * a)
+        y = ()
+
+    print(x)
